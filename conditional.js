f83a1467924477a6ef651e1edb202fb4ec7185ce
asickbappy <shahporan4256@gmail.comm>
2022-08-10 22:16:00 -0700

    code push

---
+++ conditional.js
@@ -0,0 +1,20 @@
+var biscuitPrice = 10;
+if(biscuitPrice == 15){
+    console.log("i will eat niscuit");
+}
+else{
+  console.log("biscuit nai");
+}
+var jobPaiso = true;
+var savingAmount = 5000;
+if(jobPaiso == true && savingAmount > 2000){
+    console.log("you are perfect for marrige")
+}
+else if(jobPaiso = true){
+  console.log("you had gived one chance")
+  }
+
+else{
+    console.log("you are rejected")
+}
+
